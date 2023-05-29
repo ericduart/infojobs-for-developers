177e19f9fa204e206583d401f48278be57706cbc
Eric Duart <eric.duasan@gmail.com>
2023-05-29 23:52:22 +0200

    fixed location error

--- app/offers/info/page.jsx
+++ app/offers/info/page.jsx
@@ -1,10 +1,13 @@
 'use client'
 import { useRouter } from 'next/navigation'
+import { useEffect } from 'react'
 
 export default function InfoOffer () {
   const router = useRouter()
 
-  router.push('/offers')
+  useEffect(() => {
+    router.push('/offers')
+  }, [])
 
   return (
     <h1>Cargando...</h1>
